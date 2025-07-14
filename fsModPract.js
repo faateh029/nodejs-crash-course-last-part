dcaef6765718e7f4d78e9801ae331f0c1ce263f0
faateh029 <buttfatih691@gmail.com>
2025-07-14 17:55:02 +0500

    Integrated fs module with the basic API to log few things

---
+++ fsModPract.js
@@ -0,0 +1,35 @@
+// import fs from 'fs/promises';
+
+
+// // fs.writeFile('./test.txt' , 'content from writeFile() function' , ()=>{  
+// // })
+
+
+// const readFile = async ()=>{
+//     try{
+//     const data = await fs.readFile('./test.txt' , 'utf-8')
+//     console.log(data);
+//        }catch(error){
+//         console.log(error)
+//        }
+// }
+// const writeFile= async()=>{
+//     try{   
+//        await fs.writeFile('./test.txt' , 'from write' )   
+//     }catch(error)
+//         {console.log(error)};
+// }
+
+// const appendFile= async ()=>{
+//     try{
+//        await fs.appendFile('./.txt' , '\nfrom append file')
+          
+//     }catch(error){
+//        console.log(error);
+//     }
+    
+// }
+// writeFile();
+// appendFile();
+// readFile();
+
--- server2.js
+++ server2.js
@@ -1,4 +1,5 @@
 import http from 'http';
+import fs from 'fs/promises'
 const PORT= process.env.PORTTWO
 
 const users = [
@@ -8,7 +9,17 @@ const users = [
 ]
 //logger middleware it was just to keep track of the requests 
 const logger = (req,res,next)=>{
-    //console.log(req.url     )
+    //console.log(req.url)
+    const now = new Date();
+   const formatted = now.toLocaleString(); 
+     let body='';
+      req.on('data',(chunk)=>{
+         body+=chunk.toString();
+      })
+       req.on('end' ,()=>{
+         fs.appendFile('./usersData.txt' , `{\n  Request method: ${req.method}\n  URL: ${req.url}\n  Date&Time: ${formatted}\n}\n`);
+       }
+    )
    next();
 };
 
@@ -33,13 +44,29 @@ const getUsersByIdHandler=(req,res)=>{
 }
 
 
-11
+
+
 const notFoundHandler = (req,res)=>{
      res.setHeader('Content-Type', 'text/html');
     res.statusCode = 404;
     res.end('Error: either the URL is not present or the method is not allowed');
 
 }
+
+const createUserHandler = (req,res)=>{
+      let body='';
+      req.on('data',(chunk)=>{
+         body+=chunk.toString();
+      })
+       req.on('end' , ()=>{
+       const newUser = JSON.parse(body);
+       users.push(newUser);
+       res.statusCode=201;
+       res.write(JSON.stringify(users));
+       res.end()
+       })
+}
+
 
 const server = http.createServer((req, res)=>{
     logger(req, res, () => {
@@ -49,6 +76,8 @@ const server = http.createServer((req, res)=>{
                 getUsersHandler(req,res);
             }else if (req.url.match(/\/api\/users\/([0-9]+)/)&& req.method==='GET'){
                 getUsersByIdHandler(req,res);
+            }else if (req.url==='/api/users'&&req.method==='POST'){
+                createUserHandler(req,res);
             }else{
                 notFoundHandler(req,res);
             }
