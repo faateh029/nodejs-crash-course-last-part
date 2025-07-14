23225ff4acb92747046a11781a7b30e4a80de7ad
faateh029 <buttfatih691@gmail.com>
2025-07-14 19:10:42 +0500

    checked out os module

---
+++ os.js
@@ -0,0 +1,14 @@
+import os from 'os';
+
+//information about my system
+
+//userInfo()
+
+console.log(os.userInfo());
+
+
+//totalMEM()
+console.log(os.totalmem());
+
+console.log(os.freemem());
+console.log(os.cpus());
